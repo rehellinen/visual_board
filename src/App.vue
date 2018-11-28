aa2c2f18226b5357d8f71b11cf4fa2f01a2177db
rehellinen <912377791@qq.com>
2018-11-28 10:51:24 +0800

    环境搭建

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template lang="pug">
+  div#app
+    BrokenLine
+</template>
+
+<script>
+import BrokenLine from './components/BrokenLine'
+
+export default {
+  components: {
+    BrokenLine
+  }
+}
+</script>
+
+<style scoped>
+  @import "./assets/css/reset.css";
+</style>
--- src/index.js
+++ src/index.js
@@ -3,3 +3,10 @@
  *  Create By rehellinen
  *  Create On 2018/11/28 10:24
  */
+import App from './App'
+import Vue from 'vue'
+
+new Vue({
+  el: '#app',
+  render: h => h(App)
+})
