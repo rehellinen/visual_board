fbff3169c9acab85d31db3b92a43fccabad3df12
rehellinen <912377791@qq.com>
2018-11-28 10:36:29 +0800

    脚手架搭建

---
+++ build/config.js
@@ -0,0 +1,23 @@
+/**
+ *  config.js
+ *  Create By rehellinen
+ *  Create On 2018/11/10 22:55
+ */
+module.exports = {
+  useVue: false,
+  dev: {
+    // dev-server
+    host: '127.0.0.1',
+    port: 9527,
+    autoOpenBrowser: false,
+    // es-lint
+    useEslint: true,
+    // source map
+    devtool: 'cheap-module-eval-source-map',
+  },
+
+  prod: {
+
+  }
+}
+
