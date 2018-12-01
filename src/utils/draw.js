aeb81ed65b1dd6043a5529015623ada8ccf68876
rehellinen <912377791@qq.com>
2018-12-01 19:33:13 +0800

    模板

--- src/utils/draw.js
+++ src/utils/draw.js
@@ -3,7 +3,7 @@
  *  Create By rehellinen
  *  Create On 2018/11/30 9:43
  */
-import {LineData} from "../d3/data"
+import {LineData} from "./data"
 
 document.onreadystatechange = () => {
   if (document.readyState === 'complete') { //当页面加载状态为完全结束时进入
