092bc09a7d94eca133b22fd2aa8c268084325fbf
rehellinen <912377791@qq.com>
2019-01-26 11:17:44 +0800

    layer

--- src/components/Layer.vue
+++ src/components/Layer.vue
@@ -1,14 +1,34 @@
 <template lang="pug">
   div.container
-    el-row
-      el-col(:span="8")
-        p 123
-      el-col(:span="8")
-        p 123
-      el-col(:span="8")
-        p 123
-    el-row
-      p 123
+    // 上方四个图
+    el-row(:gutter="20")
+      el-col(:span="6")
+        div.up-four
+          p 123
+      el-col(:span="6")
+        div.up-four
+          p 123
+      el-col(:span="6")
+        div.up-four
+          p 123
+      el-col(:span="6")
+        div.up-four
+          p 123
+    el-row(:gutter="20")
+      // 表格以及大图
+      el-col(:span="18")
+        el-row
+          p 123
+        el-row
+          p 123
+      // 三个图
+      el-col(:span="6")
+        el-row
+          p 123
+        el-row
+          p 123
+        el-row
+          p 123
 </template>
 
 <script>
@@ -18,5 +38,7 @@
 </script>
 
 <style lang="sass" scoped>
-
+  .up-four
+    height: 200px
+  .de
 </style>
