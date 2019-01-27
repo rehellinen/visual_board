7b8e1e456d18345bdc04b8da5b576bc6dba8026c
rehellinen <912377791@qq.com>
2019-01-27 11:27:45 +0800

    input组件样式优化

--- src/components/InputWithLabel.vue
+++ src/components/InputWithLabel.vue
@@ -1,18 +1,29 @@
 <template lang="pug">
   el-row(:gutter="10")
     el-col(:span="6")
-      p 产品编号
+      p {{title}}
     el-col(:span="18")
       el-input
 </template>
 
 <script>
   export default {
+    props: {
+      title: {
+        type: String,
+        default: ''
+      }
+    }
   }
 </script>
 
 <style lang="sass" scoped>
   @import "../assets/sass/base"
+  .el-col
+    height: 40px
+    display: flex
+    justify-content: center
+    align-items: center
   p
     margin: 0
     font-size: $normal-font-size
--- src/components/MyDialog.vue
+++ src/components/MyDialog.vue
@@ -4,18 +4,25 @@
     @close="closeDialog" width="1100px")
       el-row
         el-col(:span="12")
-          el-row
+          el-row(:gutter="20")
             el-col(:span="8")
               input-with-label(title="产品编号")
             el-col(:span="8")
+              input-with-label(title="产品名称")
             el-col(:span="8")
+              input-with-label(title="规格型号")
+          el-row(:gutter="20")
+            el-col(:span="8")
+              input-with-label(title="产品数量")
+            el-col(:span="8")
+              input-with-label(title="累计数量")
+            el-col(:span="8")
+              input-with-label(title="取数时间")
           el-row
             el-col(:span="8")
-            el-col(:span="8")
-            el-col(:span="8")
-          el-row
-            el-col(:span="8")
+              input-with-label(title="累计运行")
             el-col(:span="16")
+              input-with-label(title="备注")
         el-col(:span="12")
       el-row
         el-col(:span="12")
@@ -59,4 +66,6 @@
 </script>
 
 <style lang="sass" scoped>
+  .el-row
+    margin-top: 10px
 </style>
